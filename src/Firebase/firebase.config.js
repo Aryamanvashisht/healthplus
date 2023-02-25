c1a042317657fa4ec54160204b418c58ca834b39
Aryaman <aryamanvashisht900@gmail.com>
2023-02-25 09:36:39 +0530

    first-commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,13 @@
+
+const config ={
+  apiKey: "XXXXXXXXXXXX",
+  authDomain: "app.firebaseapp.com",
+  databaseURL: "https://app.firebaseio.com",
+  // projectId: "XXXXXX2",
+  projectId :"project-94658",
+  storageBucket: "XXXXXX.appspot.com",
+  messagingSenderId: "5..........",
+  appId: "1:52807............."
+}
+
+export default config;
